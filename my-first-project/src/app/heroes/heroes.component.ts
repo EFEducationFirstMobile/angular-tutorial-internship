cced75e949a9ddec8a3ab3ec0614d45f751328c1
Moenika <moenika.chowdhury@ef.com>
2019-07-03 13:57:48 +0100

    adding services to website - messages

--- my-first-project/src/app/heroes/heroes.component.ts
+++ my-first-project/src/app/heroes/heroes.component.ts
@@ -1,26 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
-  
+  styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1, 
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
 
-  constructor() {
-    
-   }
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
